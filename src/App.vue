ac047da207de8fbc8cf3aeffa35aea952856bfeb
ziyal <wintermist3@gmail.com>
2017-07-17 10:16:02 -0700

    adjusted styling

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="app">
-    
-    <div class="container">
-
-      <img src="./assets/klingon_logo.png" class="logo">
-
-      <div class="right-container">
-        <div class="klingon-text">
-          <img src="./assets/worf.png" class="worf-klingon">
-          <img src="./assets/son.png" class="son-klingon">
-        </div>
-
-        <ul class="navbar-list">
-          <li class="navbar-item"><router-link to="/">Home</router-link></li>
-          <li class="navbar-item dots">● ● ● ●</li>
-          <li class="navbar-item"><router-link to="/culture">Culture</router-link></li>
-          <li class="navbar-item dots">● ● ● ●</li>
-          <li class="navbar-item"><router-link to="/family">Family</router-link></li>
-          <li class="navbar-item dots">● ● ● ●</li>
-          <li class="navbar-item"><router-link to="/career">Career</router-link></li>
-          <li class="navbar-item dots">● ● ● ●</li>
-          <li class="navbar-item"><router-link to="/gallery">Gallery</router-link></li>
-        </ul>
-      </div>
+    <div class="twinkling">
       
-      <hr>
+      <div class="container">
 
-      <router-view></router-view>
+        <img src="./assets/klingon_logo.png" class="logo">
 
-      <div class="footer">This website is built with data collected from <a href="http://memory-alpha.wikia.com/wiki/Worf">Memory Alpha Wiki</a></div>
+        <div class="right-container">
+          <div class="klingon-text">
+            <img src="./assets/worf.png" class="worf-klingon">
+            <img src="./assets/son.png" class="son-klingon">
+          </div>
+
+          <ul class="navbar-list">
+            <li class="navbar-item"><router-link to="/">Home</router-link></li>
+            <li class="navbar-item dots">● ● ● ●</li>
+            <li class="navbar-item"><router-link to="/culture">Culture</router-link></li>
+            <li class="navbar-item dots">● ● ● ●</li>
+            <li class="navbar-item"><router-link to="/family">Family</router-link></li>
+            <li class="navbar-item dots">● ● ● ●</li>
+            <li class="navbar-item"><router-link to="/career">Career</router-link></li>
+            <li class="navbar-item dots">● ● ● ●</li>
+            <li class="navbar-item"><router-link to="/gallery">Gallery</router-link></li>
+          </ul>
+        </div>
+        
+        <hr>
+
+        <router-view></router-view>
+
+        <div class="footer">This website is built with data collected from <a href="http://memory-alpha.wikia.com/wiki/Worf">Memory Alpha Wiki</a>
+        </div>
+        
+      </div>
+
+      
     </div>
   </div>
 </template>
@@ -45,9 +51,74 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
   color: #2c3e50;
   background-color: #333333;
+}
+
+/* ============================================================================================== 
+STAR BACKGROUND EFFECT FROM:
+Nick Mkrtchyan
+https://www.facebook.com/sonick.pk
+http://www.Mkrtchyan.zz.mu
+================================================================================================= */
+
+@keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-webkit-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-moz-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+@-ms-keyframes move-twink-back {
+    from {background-position:0 0;}
+    to {background-position:-10000px 5000px;}
+}
+
+@keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-webkit-keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-moz-keyframes move-clouds-back {
+    from {background-position:0 0;}
+    to {background-position:10000px 0;}
+}
+@-ms-keyframes move-clouds-back {
+    from {background-position: 0;}
+    to {background-position:10000px 0;}
+}
+
+#app {
+  background:#000 url(http://www.script-tutorials.com/demos/360/images/stars.png) repeat top center;
+  z-index:0;
+}
+
+.twinkling{
+  background:transparent url(http://www.script-tutorials.com/demos/360/images/twinkling.png) repeat top center;
+  z-index:1;
+  -moz-animation:move-twink-back 500s linear infinite;
+  -ms-animation:move-twink-back 500s linear infinite;
+  -o-animation:move-twink-back 500s linear infinite;
+  -webkit-animation:move-twink-back 500s linear infinite;
+  animation:move-twink-back 500s linear infinite;
+}
+
+.clouds{
+  background:transparent url(http://www.script-tutorials.com/demos/360/images/clouds3.png) repeat top center;
+  z-index:3;
+  -moz-animation:move-clouds-back 200s linear infinite;
+  -ms-animation:move-clouds-back 200s linear infinite;
+  -o-animation:move-clouds-back 200s linear infinite;
+  -webkit-animation:move-clouds-back 200s linear infinite;
+  animation:move-clouds-back 200s linear infinite;
 }
 
 .container {
@@ -117,14 +188,11 @@ a:hover {
   text-decoration: none;
 }
 
-
 .logo {
   display: inline-block;
   width: 300px;
   vertical-align: top;
 }
-
-
 
 h1 {
   text-align: center;
@@ -135,7 +203,7 @@ h1 {
 /* FOOTER */
 .footer {
   width: 1000px;
-  background-color: black;
+  background-color: #383838;
   margin-left: -30px;
   padding-top: 20px;
   padding-bottom: 20px;
--- src/components/Career.vue
+++ src/components/Career.vue
@@ -2,21 +2,25 @@
   <div class="career">
 
     <h1>Career</h1>
+
+    <img src="../assets/tng_crew.jpg" class="img-crew">
+
+
     <p>In 2364, Lieutenant junior grade Worf was assigned as a command division bridge officer on the USS Enterprise (NCC-1701-D), under the command of Captain Jean-Luc Picard. Worf spent most of his first year on the Enterprise-D as a relief officer for the conn and other bridge stations.</p>
-
-    <p>Worf was permitted a variation from the Starfleet uniform dress code, and wore a Klingon warrior's sash, sometimes called a baldric by Humans, over his regular duty uniform.  Worf's quarters were on Deck 7, in Section 25 Baker until 2370, when he moved to Deck 2, Room 2713.</p>
-
-    <div class="flying-container">
-        <img src="../assets/enterprise.png" id="enterprise" v-on:mouseover="moveEnterprise">
-    </div>
-
-    <p>Following the death of Natasha Yar at the hands of the Armus entity, Worf became acting security chief. In 2365, Worf transferred to the operations division and officially became the Enterprise-D's chief tactical officer and security chief. He was promoted to the rank of full lieutenant in 2366. After seven years of service aboard the starship, Worf rose in rank to lieutenant commander in 2371.</p>
 
     <div class="tricorder-container">
         <img src="../assets/tricorder.png" id="tricorder" v-on:mouseover="playTricorder">
     </div>
 
+    <p>Worf was permitted a variation from the Starfleet uniform dress code, and wore a Klingon warrior's sash, sometimes called a baldric by Humans, over his regular duty uniform.  Worf's quarters were on Deck 7, in Section 25 Baker until 2370, when he moved to Deck 2, Room 2713.</p>
+
+
+    <p>Following the death of Natasha Yar at the hands of the Armus entity, Worf became acting security chief. In 2365, Worf transferred to the operations division and officially became the Enterprise-D's chief tactical officer and security chief. He was promoted to the rank of full lieutenant in 2366. After seven years of service aboard the starship, Worf rose in rank to lieutenant commander in 2371.</p>
+
     <p>During these years of service, Worf's record was marred by a single reprimand, earned when he killed Duras in an honor duel after the latter had Worf's mate, K'Ehleyr, killed. Killing Duras directly affected the ascension of a new Klingon Chancellor after the death of K'mpec.</p>
+    <div class="flying-container">
+        <img src="../assets/enterprise.png" id="enterprise" v-on:mouseover="moveEnterprise">
+    </div>
 
   </div>
 </template>
@@ -51,12 +55,20 @@ export default {
 
 <style scoped>
 
+
+.img-crew {
+  float: left;
+  width: 300px;
+  margin-right: 15px;
+  border: solid black 3px;
+}
+
 /* FLYING ENTERPRISE */
 .flying-container {
     position: relative;
     width: 1000px;
     height: 80px;
-    margin-top: 35px;
+    // margin-top: 35px;
 }
 
 #enterprise {
@@ -66,18 +78,16 @@ export default {
 
 /* PLAY TRICORDER */
 .tricorder-container {
-    position: relative;
-    width: 250px;
-    height: 170px;
+    float: right;
+    width: 100px;
 }
 
 #tricorder {
-    position: absolute;
-    width: 150px;
+    width: 90px;
 }
 
 #tricorder:hover {
-    transform: scale(1.3);
+    transform: scale(1.2);
 
 }
 
